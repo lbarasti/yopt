510de24620317b6925a42b778b79b5004be541a3
lorenzo.barasti <lorenzo.barasti@ocado.com>
2016-06-11 10:32:55 +0100

    Implement Try - first iteration

--- test/yopt_test.rb
+++ test/yopt_test.rb
@@ -251,3 +251,20 @@ describe 'None' do
     None.wont_equal Some.new(rand(100))
   end
 end
+describe 'Try' do
+  it 'something' do
+    Try{'hello'}.must_equal Success.new('hello')
+    Try{1/0}.error.must_be_kind_of ZeroDivisionError
+    lambda{Try{1/0}.get}.must_raise RuntimeError
+    Try{raise TypeError}.must_be_kind_of Failure
+    Try{raise TypeError}.error.must_be_kind_of TypeError
+    case Try{raise TypeError.new("Wrong Type")}
+    when Failure then :ok
+    else fail
+    end
+    case Try{raise TypeError.new("Wrong Type")}
+    when Failure.new(TypeError) then :ok
+    else fail
+    end
+  end
+end
