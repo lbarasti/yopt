5f8bd890295c1d79b946173713b0fc03e2a617c9
lorenzo.barasti <lorenzo.barasti@ocado.com>
2016-06-22 12:51:39 +0100

    Move Ytry related code to separate files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'yopt'
+require 'ytry'
 
 require 'minitest/autorun'
 require 'minitest/unit'
---
+++ test/ytry_test.rb
@@ -0,0 +1,27 @@
+require_relative 'test_helper'
+
+include Ytry
+
+describe 'Ytry module' do
+  it 'should have a version' do
+    refute_nil ::Ytry::VERSION
+  end
+end
+
+describe 'Try' do
+  it 'something' do
+    Try{'hello'}.must_equal Success.new('hello')
+    Try{1/0}.error.must_be_kind_of ZeroDivisionError
+    lambda{Try{1/0}.get}.must_raise RuntimeError
+    Try{raise TypeError}.must_be_kind_of Failure
+    Try{raise TypeError}.error.must_be_kind_of TypeError
+    case Try{raise TypeError.new("Wrong Type")}
+    when Failure then :ok
+    else fail
+    end
+    case Try{raise TypeError.new("Wrong Type")}
+    when Failure.new(TypeError) then :ok
+    else fail
+    end
+  end
+end
