cbb40a8398b8e66e79a91656deefc2fbb30946f8
lorenzo.barasti <lorenzo.barasti@ocado.com>
2016-06-22 15:11:09 +0100

    Split ytry tests

--- test/ytry_test.rb
+++ test/ytry_test.rb
@@ -9,12 +9,23 @@ describe 'Ytry module' do
 end
 
 describe 'Try' do
-  it 'something' do
+  it 'should wrap a successful computation into Success' do
     Try{'hello'}.must_equal Success.new('hello')
-    Try{1/0}.error.must_be_kind_of ZeroDivisionError
-    lambda{Try{1/0}.get}.must_raise ZeroDivisionError
+    
+  end
+  it 'should wrap an exception into Failure when an exception is raised' do
     Try{raise TypeError}.must_be_kind_of Failure
+  end
+end
+
+describe 'Failure' do
+  it 'should raise an exception on #get' do
+    -> { Try{1/0}.get }.must_raise ZeroDivisionError
+  end
+  it 'should return the wrapped exception on #error' do
     Try{raise TypeError}.error.must_be_kind_of TypeError
+  end
+  it 'should support case statements' do
     case Try{raise TypeError.new("Wrong Type")}
     when Failure then :ok
     else fail
